ca5f51bb2a8c264789ff792cb044c1205537eb89
Robo <roberto.garaffa@swergroup.com>
2025-10-30 16:35:22 +0100

    refactor: remove ThemesTest component from RootLayout for cleaner structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { BookProvider } from "@/components/providers/book-provider";
 import { ImageCacheProvider } from "@/components/providers/image-cache-provider";
 import { Toaster } from 'sonner'
 import "./globals.css";
-import { ThemesTest } from "@/components/test/ThemesTest";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +35,6 @@ export default function RootLayout({
             <ImageCacheProvider>
               <BookProvider>
                 {children}
-                <ThemesTest />
               </BookProvider>    
             </ImageCacheProvider>
           </LanguageProvider>
